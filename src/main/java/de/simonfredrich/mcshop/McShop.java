7a1a996c2f99090a8955c33534cb3ac70541e41d
SimonFredrich <21simplex02@gmail.com>
2021-02-03 20:59:10 +0100

    add block type functionality

--- src/main/java/de/simonfredrich/mcshop/McShop.java
+++ src/main/java/de/simonfredrich/mcshop/McShop.java
@@ -5,10 +5,17 @@ import de.simonfredrich.mcshop.commands.HealCommand;
 import de.simonfredrich.mcshop.commands.HurtCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public final class McShop extends JavaPlugin implements Listener {
@@ -20,11 +27,14 @@ public final class McShop extends JavaPlugin implements Listener {
         INSTANCE = this;
     }
 
+    public ArrayList<Material> configPickaxeBlocks = new ArrayList<Material>();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
 
         this.register();
+        this.load_config();
 
         log("has loaded.");
     }
@@ -44,9 +54,23 @@ public final class McShop extends JavaPlugin implements Listener {
         Bukkit.getConsoleSender().sendMessage(PREFIX+text);
     }
 
+    public void log_string_list(List<String> string_list) {
+        for (String i : string_list)
+        Bukkit.getConsoleSender().sendMessage(PREFIX+i);
+    }
+
     private void register() {
         Bukkit.getPluginCommand("heal").setExecutor(new HealCommand());
         Bukkit.getPluginCommand("hurt").setExecutor(new HurtCommand());
         Bukkit.getPluginManager().registerEvents(new BlockCrusher(), this);
+    }
+
+    private void load_config() {
+        List<String> loadPickaxeBlocks = McShop.this.getConfig().getStringList("blocks.pickaxe-blocks");
+        for(String s: loadPickaxeBlocks) {
+            if(Material.getMaterial(s) != null){
+                configPickaxeBlocks.add(Material.getMaterial(s));
+            }
+        }
     }
 }
--- src/main/java/de/simonfredrich/mcshop/block_interaction/BlockCrusher.java
+++ src/main/java/de/simonfredrich/mcshop/block_interaction/BlockCrusher.java
@@ -1,8 +1,10 @@
 package de.simonfredrich.mcshop.block_interaction;
 
+import de.simonfredrich.mcshop.McShop;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +13,10 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.Objects;
 
 public class BlockCrusher implements Listener {
 
@@ -19,6 +25,30 @@ public class BlockCrusher implements Listener {
     @EventHandler
     public void playerBreak(PlayerInteractEvent e) {
         block_face = e.getBlockFace();
+        Player player_interact = e.getPlayer();
+        Block block = e.getClickedBlock();
+        check_not_bohr(block, player_interact);
+    }
+
+    private void check_not_bohr(Block block, Player player_interact) {
+        for (Material m : McShop.INSTANCE.configPickaxeBlocks)
+            try {
+                if (block.getType().equals(m)) {
+                    slow_bohr(player_interact);
+                }
+            } catch (NullPointerException np) {
+                System.out.print("NullPointer because of block type.");
+            }
+
+    }
+
+    private void slow_bohr(Player player) {
+        ItemStack player_item = player.getInventory().getItemInMainHand();
+        String display_name = player_item.getItemMeta().getDisplayName();
+        Material item_material = player_item.getType();
+        if (display_name.equals("Bohrer") && item_material.equals(Material.DIAMOND_PICKAXE)) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 20, 3));
+        }
     }
 
     @EventHandler
@@ -37,74 +67,82 @@ public class BlockCrusher implements Listener {
         }
     }
 
-    private void block_crusher_xy(Block block, Player player, GameMode game_mode) {
-        ItemStack stone_axe = new ItemStack(Material.STONE_AXE);
-        if (player.getInventory().getItemInMainHand().equals(stone_axe)) {
-            int x = block.getX() - 1;
-            int y = block.getY() + 1;
-            int z = block.getZ();
-            World world = block.getWorld();
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    Location loc = new Location(world, x, y, z);
-                    if (game_mode.equals(GameMode.CREATIVE)) {
-                        loc.getBlock().setType(Material.AIR);
-                    } else {
-                        loc.getBlock().breakNaturally();
-                    }
-                    y = y - 1;
-                }
-                x = x + 1;
-                y = block.getY() + 1;
+    private void block_crusher(Block block, Player player, GameMode game_mode, String orientation) {
+        ItemStack player_item = player.getInventory().getItemInMainHand();
+        String display_name = player_item.getItemMeta().getDisplayName();
+        Material item_material = player_item.getType();
+        if (display_name.equals("Bohrer") && item_material.equals(Material.DIAMOND_PICKAXE)) {
+            int x = 0, y = 0, z = 0;
+            switch (orientation) {
+                case "xy":
+                    x = block.getX() - 1;
+                    y = block.getY() + 1;
+                    z = block.getZ();
+                    break;
+                case "yz":
+                    x = block.getX();
+                    y = block.getY() + 1;
+                    z = block.getZ() - 1;
+                    break;
+                case "xz":
+                    x = block.getX() + 1;
+                    y = block.getY();
+                    z = block.getZ() + 1;
+                    break;
             }
-        }
-    }
 
-    private void block_crusher_yz(Block block, Player player, GameMode game_mode) {
-        ItemStack stone_axe = new ItemStack(Material.STONE_AXE);
-        if (player.getInventory().getItemInMainHand().equals(stone_axe)) {
-            int x = block.getX();
-            int y = block.getY() + 1;
-            int z = block.getZ() - 1;
             World world = block.getWorld();
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    Location loc = new Location(world, x, y, z);
-                    if (game_mode.equals(GameMode.CREATIVE)) {
-                        loc.getBlock().setType(Material.AIR);
-                    } else {
-                        loc.getBlock().breakNaturally();
-                    }
-                    y = y - 1;
-                }
-                z = z + 1;
-                y = block.getY() + 1;
+            boolean broken_block_mat = false;
+            for (Material m : McShop.INSTANCE.configPickaxeBlocks) {
+                if (block.getType().equals(m)) broken_block_mat = true;
             }
-        }
-    }
 
-    private void block_crusher_xz(Block block, Player player, GameMode game_mode) {
-        ItemStack stone_axe = new ItemStack(Material.STONE_AXE);
-        if (player.getInventory().getItemInMainHand().equals(stone_axe)) {
-            int x = block.getX() + 1;
-            int y = block.getY();
-            int z = block.getZ() + 1;
-            World world = block.getWorld();
+            boolean unbreaking_case = true;
 
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     Location loc = new Location(world, x, y, z);
-                    if (game_mode.equals(GameMode.CREATIVE)) {
-                        loc.getBlock().setType(Material.AIR);
-                    } else {
-                        loc.getBlock().breakNaturally();
+                    for (Material m : McShop.INSTANCE.configPickaxeBlocks) {
+                        if (loc.getBlock().getType().equals(m) && broken_block_mat) {
+                            if (game_mode.equals(GameMode.CREATIVE)) {
+                                loc.getBlock().setType(Material.AIR);
+                            } else {
+                                loc.getBlock().breakNaturally();
+                                if (player_item.containsEnchantment(Enchantment.DURABILITY) && unbreaking_case) {
+                                    unbreaking_case = false;
+                                } else {
+                                    Damageable meta = (Damageable) player_item.getItemMeta();
+                                    int damage = meta.getDamage();
+                                    meta.setDamage(damage+1);
+                                    player_item.setItemMeta((ItemMeta) meta);
+                                }
+                            }
+                        }
                     }
-                    x = x - 1;
+                    switch (orientation) {
+                        case "xy":
+                        case "yz":
+                            y = y - 1;
+                            break;
+                        case "xz":
+                            x = x - 1;
+                            break;
+                    }
                 }
-                z = z - 1;
-                x = block.getX() + 1;
+                switch (orientation) {
+                    case "xy":
+                        x = x + 1;
+                        y = block.getY() + 1;
+                        break;
+                    case "yz":
+                        z = z + 1;
+                        y = block.getY() + 1;
+                        break;
+                    case "xz":
+                        z = z - 1;
+                        x = block.getX() + 1;
+                        break;
+                }
             }
         }
     }
@@ -114,15 +152,15 @@ public class BlockCrusher implements Listener {
         switch (block_face.name()) {
             case "NORTH":
             case "SOUTH":
-                block_crusher_xy(block, player, game_mode);
+                block_crusher(block, player, game_mode, "xy");
                 break;
             case "EAST":
             case "WEST":
-                block_crusher_yz(block, player, game_mode);
+                block_crusher(block, player, game_mode, "yz");
                 break;
             case "UP":
             case "DOWN":
-                block_crusher_xz(block, player, game_mode);
+                block_crusher(block, player, game_mode, "xz");
                 break;
         }
     }
